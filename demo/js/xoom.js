4c37bc26968c5d71274a8603f6eb8798d78b5993
System Administrator <root@Eliads-MacBook-Pro.local>
2016-04-20 18:13:42 -0400

    Initial commit

---
+++ demo/js/xoom.js
@@ -0,0 +1,32 @@
+/* XOOM! (alpha 1.0) | MIT
+ * Build: 
+ */
+
+var dropDown = document.querySelectorAll('.dropDown');
+var dropDownArray = Array.prototype.slice.call(dropDown,0);
+
+dropDownArray.forEach(function(el){
+    var div = el.querySelector("div");
+    el.onmouseover=function(){
+        TweenMax.to(div, .12, {
+            opacity: 1,
+            top: 59,
+            autoAlpha: 1,
+            scaleX: 1,
+            scaleY: 1,
+            repeat: 0,
+            ease: Power1.easeInOut
+        });
+    };
+    el.onmouseleave=function(){
+        TweenMax.to(div, .12, {
+            opacity: 0,
+            autoAlpha:0,
+            scaleX: 0.9,
+            scaleY: 0.95,
+            top: 40,
+            repeat: 0,
+            ease: Power1.easeInOut
+        });
+    };
+});
